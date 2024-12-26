9c716397dbab9901c5cf54795129b05a69a15476
user <user@DESKTOP-DV6KRKO>
2024-12-26 20:22:23 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassLibrary
+{
+    public class Circle
+    {
+        public double Radius { get; private set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public bool IsPointInside(double x, double y)
+        {
+            double distanceSquared = x * x + y * y;
+            double radiusSquared = Radius * Radius;
+
+            return distanceSquared <= radiusSquared;
+        }
+    }
+}
---
+++ nasl/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите координату x1: ");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите координату y1: ");
+        double y1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(" Введите радиус окружности: ");
+        double z1 = double.Parse(Console.ReadLine());
+
+        Circle circle = new Circle(z1); 
+
+        bool isInside = circle.IsPointInside(x1, y1);
+        if (isInside)
+        {
+            Console.WriteLine("Точка находится внутри окружности.");
+        }
+        else
+        {
+            Console.WriteLine("Точка находится вне окружности.");
+        }
+    }
+}
